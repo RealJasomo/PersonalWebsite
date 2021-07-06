b772eaf578a9dd200b6ece1343fd986720980fb1
Jason Cramer <jason@Jasons-MacBook-Pro.local>
2021-07-05 22:54:49 -0400

    started home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
 import FrostedGlass from '@components/FrostedGlass'
 import Head from 'next/head'
+import Image from 'next/image'
+
+
+import me from '@res/me.png'
 
 import styles from './index.module.scss'
 
@@ -7,9 +11,20 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Jason Cramer</title>
+        <title>Jason Cramer - Software Engineer</title>
         <meta name="description" content="Jason Cramer - Full stack engineer" />
       </Head>
+      <div className={styles.main}>
+        <div className={styles.aside}>
+          <Image className={styles.image} src={me} />
+        </div>
+        <div className={styles.paragraph}>
+          <FrostedGlass> 
+            <h1>Hi there!</h1>
+            <p>I am a <b>full stack</b> software engineer</p>
+          </FrostedGlass>          
+        </div>
+      </div>
     </div>
   )
 }
